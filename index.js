a91c4a68a17741b7b799c43055bbb30d1cd8eefa
koldin108902 <alex108902@naver.com>
2021-05-02 20:10:27 +0900

    socket과 cookie를 동시에 쓰는 방법 모색중

--- index.js
+++ index.js
@@ -2,12 +2,31 @@ const app = require('express')()
 const port = 3004
 const server = app.listen(port, () => { console.log(`socket is running on 3004`) })
 const io = require('socket.io').listen(server)
+const bp = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
 const fs = require('fs')
+//쿠키로 서버 명을 저장, 세션으로 로그인 유무 확인.
 
 const index = require('./')
 
+//-----------------------
+
+app.use(cookieParser())
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store:new FileStore()
+}))
+
+app.post('*', bp.urlencoded({ extended: false}))
+
 app.set('views', __dirname + '/public')
 app.set('view engine','ejs')
+
+//-----------------------
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
@@ -16,11 +35,18 @@ app.get('/', (req, res) => {
 io.on('connect', (socket) => {
     let room = JSON.parse(fs.readFileSync('F:/문서/node.js/경매/json/room.json').toString())
     
-    socket.on('joinRoom', (name) => {
+    socket.on('joinRoom', async (name) => {
         for(_room in room.main) {
             console.log(room.main[_room])
             if(room.main[_room] == name) {
-                return socket.join(_room)
+                let cookie = undefined
+                await app.get('*', (req, res) => {
+                    res.cookie('room', _room, { httpOnly: true })
+                    cookie = req.cookie.room
+                })
+                console.log(cookie)
+                socket.join(_room)
+                return socket.emit('joined')
             }
         }
         socket.emit('noneRoom')
